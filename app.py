2c2be59395d745923e5d00baa80e4f10f20e2186
Jeremy Hetzel <jthetzel@openiii.com>
2017-04-30 09:17:53 -0230

    Change 404 to route to index

--- app.py
+++ app.py
@@ -64,16 +64,11 @@ def send_static_panel(path):
     return send_from_directory(join(app._static_folder, 'panel'), path)
 
 
+@app.errorhandler(404)
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('index.html')
-
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
 
 
 @app.route('/api', methods=['GET', 'POST'])
@@ -107,12 +102,6 @@ def api():
 # The functions below should be applicable to all Flask apps.
 ###
 
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
 
 @app.after_request
 def add_header(response):
@@ -123,12 +112,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
